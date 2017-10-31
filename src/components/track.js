cdac63e285bc611812793d066babb9e1a2a9c25b
LkeMitchll <mail@luke.fail>
2017-10-31 08:48:12 +0000

    Basic styling of dates

--- src/components/track.js
+++ src/components/track.js
@@ -7,7 +7,7 @@ class Track extends React.Component {
     return (
       <li key={this.props.id}>
         <a className={styles.link} href={this.props.url}>{this.props.artist} - {this.props.name}</a>
-        <span>{formatDate(this.props.date)}</span>
+        <span className={styles.date}>{formatDate(this.props.date)}</span>
       </li>
     )
   }
