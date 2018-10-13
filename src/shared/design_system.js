43ebfba5a4db6a0747663b1df427cc538d8fec3d
LkeMitchll <mail@luke.fail>
2018-10-13 17:09:08 +0100

    Remove cruft from variables

--- src/shared/design_system.js
+++ src/shared/design_system.js
@@ -9,10 +9,6 @@ const ds = {
       base: '"Lunchtype 24", Times New Roman',
       alt: '"Druk XX Condensed", Courier, Monaco, monospace',
     },
-    fontWeight: {
-      base: 400,
-      bold: 600,
-    },
   },
   colors: {
     dark: 'black',
@@ -25,13 +21,10 @@ const ds = {
     s: '30rem',
   },
   spacing: {
-    xs: '0.4rem',
     s: '0.8rem',
     base: '1rem',
     l: '2rem',
-    alt: '1.2rem',
   },
-  measure: '30rem',
 }
 
 export default ds
